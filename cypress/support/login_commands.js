123f9458dc65cc4624e6f2ab3d971977ad7d3d0a
vinnybp <vihgamerr@hotmail.com>
2025-01-08 00:36:53 -0300

    refactor:  indentation of loginSuccess Commands

--- cypress/support/login_commands.js
+++ cypress/support/login_commands.js
@@ -1,6 +1,6 @@
 ///  <reference types="Cypress"/>
 
-Cypress.Commands.add('loginSuccess',()=>{
+Cypress.Commands.add('loginSuccess', () => {
     cy.visit("https://www.saucedemo.com/v1")
     cy.get('[data-test="username"]').type("standard_user")
     cy.get('[data-test="password"]').type("secret_sauce")
